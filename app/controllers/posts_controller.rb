ad457628aa1617968430927c18a766c4d5e9181d
yusuke3939 <y.t.041990@gmail.com>
2020-04-11 23:40:18 +0900

    add edit and delete function

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -21,7 +21,19 @@ class PostsController < ApplicationController
     @post = Post.find(params[:id])
   end
 
-  
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to post_path(post.id)
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to root_path
+  end
+
+
 
 
   private
